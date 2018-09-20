5a7da8efc9462d9a8cc268021eacd8edbb989096
kstoneham <kevin.g.stoneham@gmail.com>
2018-09-19 21:25:33 -0500

    added files"

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt-nodej");
+const bcrypt = require("bcrypt-nodejs");
 
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
